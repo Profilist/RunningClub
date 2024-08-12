222888d1bca1324d6c3779ab85b525b746e7c2cf
Profilist <84641209+Profilist@users.noreply.github.com>
2024-08-12 14:47:08 -0400

    Added main webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const handleRegister = () => {
+    window.location.href = "https://www.google.com";
+  };
+
+  const handleInstagram = () => {
+    window.location.href = "https://www.instagram.com/grayslake_running_club/";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing">
+      <div className="headerBox">
+        <div className="menu">
+        </div>
+        <h1 className="title">
+          RUN LIKE A <span className="redText">CHAMPION</span>
+        </h1>
+      </div>
+      <div className="bodyBox">
+        <img className="image" src="/Running Club.jpg" alt="Running Club" />
+        <div className="text">
+          <div className="headingBody"> 
+            <div className="box"/>
+            <h2> JOIN OUR RUNS</h2> 
+          </div>
+          <p>
+            At our running club, we pride ourselves on being an inclusive and
+            welcoming community of runners. Whether you're just starting out or
+            you're a seasoned marathoner, you'll find a place with us. Our club
+            is open to runners of all races, genders, ages, and abilities. We
+            believe that running is for everyone, and we’re here to support and
+            encourage each other every step of the way.
+            <br />
+            <br />
+            How to Get Started <br />
+            <br />
+              1. Register Today: Signing up is easy and free! Simply fill out our
+            registration form to become a member. <br />
+            <br />
+              2. Join a Run: After registering, check out our upcoming runs and join
+            the one that suits you best. <br />
+            <br />
+              3. Stay Connected: Follow us on social media and join our online
+            community to stay updated on events and connect with fellow runners.
+            <br />
+            <br />
+            Don’t be shy about your running capabilities – everyone has a place
+            with us, and we’re excited to have you join our community. Lace up
+            your running shoes, and let’s hit the pavement together!
+          </p>
+          <button onClick={handleRegister} className="registerButton">REGISTER</button>
+        </div>
+      </div>
+      <div className="footerBox">
+        <h2 className="footerText">
+          READY TO <span className="redText">RUN</span>?
+        </h2>
+        <img onClick={handleInstagram} className="instagramLogo" src="/Instagram.svg" alt="Instagram" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
